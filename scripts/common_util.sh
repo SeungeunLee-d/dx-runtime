e52774c53955415ff61ff7164e41d105f781d4f4
DX-CI <dci@deepx.ai>
2025-08-20 20:28:51 +0900

    runtime-20250722-rc3+manual.sha.39da07a

--- scripts/common_util.sh
+++ scripts/common_util.sh
@@ -32,3 +32,60 @@ print_colored() {
 print_colored_v2() {
     print_colored "$2" "$1"
 }
+
+
+check_container_mode() {
+    # Check if running in a container
+    if grep -qE "/docker|/lxc|/containerd" /proc/1/cgroup || [ -f /.dockerenv ]; then
+        print_colored_v2 "INFO" "(container mode detected)"
+        return 0
+    else
+        print_colored_v2 "INFO" "(host mode detected)"
+        return 1
+    fi
+}
+
+check_virtualenv() {
+    if [ -n "$VIRTUAL_ENV" ]; then
+        venv_name=$(basename "$VIRTUAL_ENV")
+        print_colored_v2 "✅ Virtual environment '$venv_name' is currently active."
+        return 0
+    else
+        print_colored_v2 "❌ No virtual environment is currently active."
+        return 1
+    fi
+}
+
+
+# Handle command failure with user confirmation and suggested action
+handle_cmd_failure() {
+    local error_message=$1
+    local hint_message=$2
+    local origin_cmd=$3
+    local suggested_action_cmd=$4
+    
+    print_colored_v2 "ERROR" "${error_message}"
+    print_colored_v2 "HINT" "${hint_message}"
+    print_colored_v2 "YELLOW" "Would you like to perform the suggested action now? [Y/n] (Default is 'y' after 10 seconds of no input. This process will be aborted if you enter 'n')"
+    read -t 10 -p ">> " user_input
+    user_input=${user_input:-Y}
+    if [[ "${user_input,,}" == "n" ]]; then
+        print_colored_v2 "INFO" "This process aborted by user."
+        exit 1
+    else
+        if [ -n "$suggested_action_cmd" ]; then
+            print_colored_v2 "INFO" "Suggested action will be performed."
+            eval "$suggested_action_cmd" || {
+                print_colored_v2 "ERROR" "Failed to perform suggested action."
+                exit 1
+            }
+        fi
+
+        if [ -n "$origin_cmd" ]; then
+            eval "$origin_cmd" || {
+                print_colored_v2 "ERROR" "${error_message}"
+                exit 1
+            }
+        fi
+    fi
+}
